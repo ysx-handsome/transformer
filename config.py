941e59e8a490760bf7db7765fcc868194fe352f5
ysx <ysx@ysxdeMacBook-Pro.local>
2023-09-22 11:33:56 +0800

    dataloader

--- config.py
+++ config.py
@@ -2,18 +2,21 @@
 class transformer_config:
     def __init(self):
         self.transformer_config = {
-            'prediction_length':24,
-            'context_length':24*2, #same as prediction_length
-            'distribution_output':'student_t',
-            'utlize_default_lags_sequence':False,
-            'lags_sequence': [1,2,3,4,5,6,7],
-            'num_time_features':2, #year and month
-            'num_static_categorical_features':2, # 油品编码，油站编码，
-            'cardinality': -1,#待确认
-            'embedding_dimension':32,
-            'd_model':64,
-            'encoder_layers':2,
-            'decoder_layers':2,
-            'encoder_attention_heads:':2,
-            'decoder_attention_heads' :2
+            'input_size':1,
+            'dec_seq_len':4,
+            'dim_val':512,
+            'n_encoder_layers':4,
+            'n_decoder_layers':4,
+            'n_heads':4,
+            'dropout_encoder':0.2,
+            'dropout_decoder':0.2,
+            'dropout_pos_enc':0.1,
+            'dim_feedforward_encoder':2048,
+            'dim_feedforward_decoder':2048,
+            'num_predicted_feature':1,
         }
+
+        self.config = {
+            'device':'cpu'
+        }
+    
---
+++ transformer_dataset.py
@@ -0,0 +1,67 @@
+import os
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+from typing import Tuple
+
+
+class TransformerDataset(Dataset):
+
+    def __init__(self,data: torch.tensor,indices:list,enc_seq_len:int,dec_seq_len:int,
+                 target_seq_len:int)-> None:
+        super().__init__()
+
+        self.indices = indices
+        self.data = data
+
+        self.enc_seq_len = enc_seq_len
+
+        self.dec_seq_len = dec_seq_len
+
+        self.target_seq_len = target_seq_len
+
+    def __len__(self):
+
+        return len(self.indices)
+    
+    def __getitem__(self, index):
+
+        start_index = index[0]
+
+        end_index = index[1]
+
+        sequence = self.data[start_index:end_index]
+
+        src, trg, trg_y = self.get_src_trg(
+            sequence=sequence,
+            enc_seq_len=self.enc_seq_len,
+            dec_seq_len=self.dec_seq_len,
+            target_seq_len=self.target_seq_len
+            )
+
+        return src, trg, trg_y
+    
+    def get_src_trg(
+        self,
+        sequence: torch.Tensor, 
+        enc_seq_len: int, 
+        target_seq_len: int
+        ) -> list[torch.tensor, torch.tensor, torch.tensor]:
+    
+        assert len(sequence) == enc_seq_len + target_seq_len, "Sequence length does not equal (input length + target length)"
+
+        src = sequence[:enc_seq_len] 
+
+        trg = sequence[enc_seq_len-1:len(sequence)-1]
+        
+        trg = trg[:, 0]
+
+        if len(trg.shape) == 1:
+
+            trg = trg.unsqueeze(-1)
+
+        trg_y = sequence[-target_seq_len:]
+
+        trg_y = trg_y[:, 0]
+
+        return src, trg, trg_y.squeeze(-1)
